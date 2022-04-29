7b71f5b741e83f327e1c63d4dc2e4d817bcbbf4f
rohan8271 <rohan62905@gmail.com>
2022-04-29 11:42:25 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Observable + create-react-app
+
+This example has moved to https://github.com/observablehq/examples/tree/main/react-create-react-app
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Runtime, Inspector } from "@observablehq/runtime";
+import React, { useEffect, useRef } from "react";
+import sunburst from "./@d3/zoomable-sunburst";
+import "./App.css";
+
+export default function App() {
+  const ref = useRef();
+
+  useEffect(() => {
+    const runtime = new Runtime();
+    runtime.module(sunburst, (name) => {
+      if (name === "chart") {
+        return new Inspector(ref.current);
+      }
+    });
+    return () => runtime.dispose();
+  }, []);
+
+  return (
+    <>
+      <div className="test" ref={ref} />
+    </>
+  );
+}
